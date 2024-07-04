448904dbc8ff814e00b25faf39ec083c32a93a87
Kamui2134 <ctorkey@gmail.com>
2024-07-04 11:53:55 +0300

    MUSTANG PROJECT INITIALIZATION

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='block-1'>
+				<div className='block-1__box'>
+					<h1 className='block-1__h1'>
+						<span className='text-gradient'> mustang</span>
+					</h1>
+					<div className='block-1__img-container'>
+						<img className='block-1__img' src='/img/1.png' alt='mustang' />
+					</div>
+					<h3 className='block-1__h3'>
+						1969 Mustang Body on a 2014 GT500 Chassis?
+					</h3>
+					<h1 className='block-1__h2'>
+						<span className='text-gradient'>yes please!</span>
+					</h1>
+				</div>
+			</div>
+			<div className='block-2'>
+				<div className='block-2__top-triangle' />
+				<div className='block-2__box'>
+					<div className='block-2__column-1'>
+						<div className='block-2__img-container'>
+							<img className='block-2__img' src='/img/2.png' alt='mustang' />
+						</div>
+					</div>
+					<div className='block-2__column-2'></div>
+				</div>
+			</div>
+			<div className='block-3'>
+				<div className='block-3__bottom-triangle' />
+			</div>
+			<div className='block-4'></div>
+			<div className='block-5'></div>
+		</div>
+	)
 }
 
-export default App;
+export default App
